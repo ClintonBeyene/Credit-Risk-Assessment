72bd7b8e6ee980b0093eb1d0352caeaed9c6061c
ClintonBeyene <clintonbeyene52@gmail.com>
2024-10-04 15:47:23 +0300

    Add data loading scripts

---
+++ src/data_loading.py
@@ -0,0 +1,47 @@
+import numpy as np
+import gdown
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def download_file_from_google_drive(file_id, output_path):
+    """
+    Download a file from Google Drive.
+
+    Parameters:
+    file_id (str): The ID of the file to download.
+    output_path (st): The path where the file will be saved.
+    """
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, output_path, quiet=False)
+
+def load_data(file_path):
+
+    """
+    Load data from a CSV file.
+
+    Parameters: 
+    file_path (str): The path to the csv file.
+
+    Return:
+    pd.DataFrame: The loaded data as a pandas DataFrame.
+    """
+
+    data = pd.read_csv(file_path)
+    return data
+
+
+def split_data(data, test_size=0.2, random_state=42):
+    """
+    Split data into training and testing sets.
+
+    Parameters:
+    data(pd.DataFrame): The data to split.
+    test_size (float): The proportion of the data to include in the test split.
+    random_state (int): The seed used by random number generator.
+
+    Returns:
+    pd.DataFrame, pd.DataFrame: The training and testing Data. 
+    """
+
+    train_data, test_data = train_test_split(data, test_size=test_size, random_state=random_state)
+    return train_data, test_data
