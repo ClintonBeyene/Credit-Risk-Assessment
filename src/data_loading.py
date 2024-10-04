f0438238eb41609a24047cfebdb8ef40b829a54e
ClintonBeyene <clintonbeyene52@gmail.com>
2024-10-04 16:20:21 +0300

    Add data loading script

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,9 +1,10 @@
 import numpy as np
 import gdown
+import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def download_file_from_google_drive(file_id, output_path):
+def download_file_from_google_drive(file_ids, output_dir, file_names):
     """
     Download a file from Google Drive.
 
@@ -11,8 +12,10 @@ def download_file_from_google_drive(file_id, output_path):
     file_id (str): The ID of the file to download.
     output_path (st): The path where the file will be saved.
     """
-    url = f"https://drive.google.com/uc?id={file_id}"
-    gdown.download(url, output_path, quiet=False)
+    for file_id, file_name in zip(file_ids, file_names):
+        url = f"https://drive.google.com/uc?id={file_id}"
+        output_path = os.path.join(output_dir, file_name)
+        gdown.download(url, output_path, quiet=False)
 
 def load_data(file_path):
 
