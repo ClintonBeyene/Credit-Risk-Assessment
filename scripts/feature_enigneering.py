a2c595d3ecc0ab769ba7ff2973949edea4ac36e5
ClintonBeyene <clintonbeyene52@gmail.com>
2024-10-05 21:09:50 +0300

    Function to encode variables

--- scripts/feature_enigneering.py
+++ scripts/feature_enigneering.py
@@ -34,3 +34,43 @@ def extract_features(df, cols, features=None):
                 df[col + '_' + feature] = getattr(df[col].dt, feature.lower())
         
         df[col + '_Elapsed'] = df[col].astype(np.int64) // 10 ** 9
+
+# Frequency Encoding
+def encode_FE(df, cols):
+    for col in cols:
+        nm = col + '_FE'
+        df_comb = pd.concat([df[col], df[col]]).value_counts().to_dict()
+        df[nm] = df[col].map(df_comb).astype('float32')
+        print(nm, ', ', end='')
+
+# Label Encoding
+def encode_LE(df, cols):
+    for col in cols:
+        df_comb = pd.concat([df[col], df[col]], axis=0)
+        df_comb, _ = df_comb.factorize(sort=True)
+        df[col] = df_comb[:len(df)].astype('int32')
+        print(col, ', ', end='')
+
+# Group Aggregation
+def encode_AG(df, group, main_columns, aggregations):
+    for main_column in main_columns:
+        for agg_type in aggregations:
+            new_col_name = group + '_' + main_column + '_' + agg_type
+            temp_df = df.groupby(group)[main_column].agg([agg_type]).reset_index().rename(columns={agg_type: new_col_name})
+            df = df.merge(temp_df, on=group, how='left')
+            df[new_col_name] = df[new_col_name].astype('float32')
+            print(new_col_name, ', ', end='')
+
+# Combine Features
+def encode_CB(df, col1, col2):
+    nm = col1 + '_' + col2
+    df[nm] = df[col1].astype(str) + '_' + df[col2].astype(str)
+    print(nm, ', ', end='')
+
+# Group Aggregation NUnique
+def encode_AG2(df, group, main_columns):
+    for main_column in main_columns:
+        new_col_name = group + '_' + main_column + '_ct'
+        df[new_col_name] = df.groupby(group)[main_column].nunique().to_dict()
+        df[new_col_name] = df[group].map(df[new_col_name]).astype('float32')
+        print(new_col_name, ', ', end='')
